6eb8b9a32fc05ba0385de9510e2c4b084286b221
wjt1321 <williamjohnthrasher@gmail.com>
2016-12-11 00:52:28 -0500

    beginning of achievements

--- LD37.gmx/scripts/scrSendHighScore.gml
+++ LD37.gmx/scripts/scrSendHighScore.gml
@@ -4,8 +4,11 @@ var user = argument0;
 var s = argument1;
 var hash = md5_string_utf8(string(user) + string(s) + "AFlatMinerSecuritySucks9836");
 
+var f = objPersistentObject.flakes;
+var t = objPersistentObject.time;
 
-var address = SERVER_ADDRESS + "fish_ahs.php?user=" + string(user) + "&score=" + string(s) + "&other=" + string(hash);
+
+var address = SERVER_ADDRESS + "fish_ahs.php?user=" + string(user) + "&score=" + string(s) + "&other=" + string(hash) + "&flakes=" + string(f) + "&time=" + t;
 
 
 
--- code/fish_ahs.php
+++ code/fish_ahs.php
@@ -5,6 +5,9 @@ include "db.php";
 $user = $_GET["user"];
 $score = intval($_GET["score"]);
 $other = $_GET["other"];
+$flakes = $_GET["flakes"];
+$time = $_GET["time"];
+
 
 $val = md5($user.$score.$magic);
 
@@ -15,12 +18,12 @@ $dbConnection = new PDO("mysql:dbname=".$db.";host=127.0.0.1;charset=utf8", $u, 
 $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt = $dbConnection->prepare('INSERT INTO '.$scoretable.' ( user, score ) VALUES (:user, :score)');
+$stmt = $dbConnection->prepare('INSERT INTO '.$scoretable.' ( user, score, flakesEaten, timeInMS ) VALUES (:user, :score, :flakes, :time)');
 
-$stmt->execute(array('user' => $user, 'score' => $score));
+$stmt->execute(array('user' => $user, 'score' => $score, 'flakes' => $flakes, 'time' => $time));
 
 echo 'Success';
 } else {
-  echo 'Failure ',$other, '      ', $val;
+  echo 'Failure ';
 }
 ?>
